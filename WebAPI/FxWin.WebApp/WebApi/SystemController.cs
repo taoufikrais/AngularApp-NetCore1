befcb4c346c1400dde7d7027eb16773ed200cef9
SDMN01\SJ5382 <taoufik.rais1@external.engie.com>
2018-07-05 17:25:47 +0200

    add system controller to show connection string and log

---
+++ WebAPI/FxWin.WebApp/WebApi/SystemController.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Data.Entity;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Web.Http;
+using System.Web.Http.Description;
+using FXWIN.Data.Provider;
+
+namespace FxWin.WebApp.WebApi
+{
+    [RoutePrefix("api/System")]
+    public class SystemController : ApiController
+    {
+        [HttpGet]
+        [AllowAnonymous]
+        [Route("GetConnexionString")]
+        public async Task<IHttpActionResult> GetConnexionString()
+        {
+            System.Configuration.Configuration rootWebConfig = System.Web.Configuration.WebConfigurationManager.OpenWebConfiguration("/FxWin.WebApp");
+            System.Configuration.ConnectionStringSettings connString;
+            if (rootWebConfig.ConnectionStrings.ConnectionStrings.Count > 0)
+            {
+                connString = rootWebConfig.ConnectionStrings.ConnectionStrings["FXWinV2Entities"];
+                if (connString != null)
+                    return Ok(connString.ConnectionString);
+                else
+                    return NotFound();
+            }
+            return NotFound();
+        }
+
+        [HttpGet]
+        [AllowAnonymous]
+        [Route("GetLogs")]
+        public async Task<IHttpActionResult> GetLogs()
+        {
+            System.Configuration.Configuration rootWebConfig = System.Web.Configuration.WebConfigurationManager.OpenWebConfiguration("/FxWin.WebApp");
+            System.Configuration.ConnectionStringSettings connString;
+            if (rootWebConfig.ConnectionStrings.ConnectionStrings.Count > 0)
+            {
+                connString = rootWebConfig.ConnectionStrings.ConnectionStrings["FXWinV2Entities"];
+                if (connString != null)
+                    return Ok(connString.ConnectionString);
+                else
+                    return NotFound();
+            }
+            return NotFound();
+        }
+
+
+    }
+}
